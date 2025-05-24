26f1e96e0fe3929a58f40930720ebe6321937b44
ahmadsopiyan27 <ahmmadsopiyann@gmail.com>
2025-05-24 13:42:44 +0700

    update revamp

--- Basic.cy.js
+++ Basic.cy.js
@@ -2,7 +2,7 @@ describe('e2e test saucedemo', function(){
     it('success', function(){
         
         //visit website
-        cy.visit('https://www.saucedemo.com');
+        cy.visit('/');
 
         //login
         cy.xpath(`//input[@data-test='username']`).type('visual_user');
